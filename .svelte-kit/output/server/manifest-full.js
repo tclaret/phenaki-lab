39d0650dfd5b2a313577d8e4b29da81830d187e3
tclaret <tclaret@gmail.com>
2025-12-10 21:48:06 +0100

    Configure GitHub

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.CA5M6LUI.js",app:"_app/immutable/entry/app.VdHYnt5L.js",imports:["_app/immutable/entry/start.CA5M6LUI.js","_app/immutable/chunks/D8a4VFuf.js","_app/immutable/chunks/zw_chER5.js","_app/immutable/chunks/BQFw1Q7y.js","_app/immutable/entry/app.VdHYnt5L.js","_app/immutable/chunks/zw_chER5.js","_app/immutable/chunks/CfBDm7bY.js","_app/immutable/chunks/BPwTt6vP.js","_app/immutable/chunks/BQFw1Q7y.js","_app/immutable/chunks/BuQu0Dv8.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.CLb_U_Gx.js",app:"_app/immutable/entry/app.BDh5sNED.js",imports:["_app/immutable/entry/start.CLb_U_Gx.js","_app/immutable/chunks/BuM9_pAX.js","_app/immutable/chunks/zw_chER5.js","_app/immutable/chunks/BQFw1Q7y.js","_app/immutable/entry/app.BDh5sNED.js","_app/immutable/chunks/zw_chER5.js","_app/immutable/chunks/CfBDm7bY.js","_app/immutable/chunks/BPwTt6vP.js","_app/immutable/chunks/BQFw1Q7y.js","_app/immutable/chunks/BuQu0Dv8.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
